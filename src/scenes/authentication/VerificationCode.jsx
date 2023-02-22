be4df8cb991b0129ca33af5cd2427b58584c3378
sonamnayak <sonamnayak2710@gmail.com>
2023-02-23 03:57:10 +0530

    added verification code component

---
+++ src/scenes/authentication/VerificationCode.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import img from '../../assets/Frame 626200.svg'
+import lock from '../../assets/lock_24px.svg'
+
+const VerificationCode = () => {
+  return (
+    <div className="verification">
+        <div className="welcome-img">
+                <img src={img} alt="authImg" />
+            </div>
+
+            <div className="welcome-desc">
+                <div className="box1">
+                    <div className="back"><i class="fa-solid fa-angle-left"></i>Back</div>
+                    <div className="onboarding">
+                        <p>Almost there!</p>
+                        <h5>Onboarding</h5>
+                    </div>
+                </div>
+                <h2>Verification Code</h2>
+                <p>One final step for the goals and dream that you have been waiting for.</p>
+                <label htmlFor="phone">OTP</label> <br />
+                <div className="phone-number">
+                    <input type="number" name="" id="phone" placeholder="Enter OTP" autoComplete="off" />
+                </div>
+                <button>Verify</button>
+                <div className="secure"><img src={lock} alt="lock" />Your Info is safely secured</div>
+            </div>
+    </div>
+  )
+}
+
+export default VerificationCode
--- src/scenes/authentication/index.jsx
+++ src/scenes/authentication/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Platform from './Platform'
 import './index.css'
 import WelcomeBack from './WelcomeBack'
+import VerificationCode from './VerificationCode'
 
 const index = () => {
   return (
     <div>
         <Platform />
         <WelcomeBack />
+        <VerificationCode />
     </div>
   )
 }
